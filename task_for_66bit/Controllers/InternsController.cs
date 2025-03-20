35f65e3411292d2ac53377f2d9f24cb0554b2160
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2025-03-20 22:50:01 +0300

    Добавил Controllers

---
+++ task_for_66bit/Controllers/InternsController.cs
@@ -0,0 +1,62 @@
+using task_for_66bit.Data.Models;
+using task_for_66bit.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace task_for_66bit.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InternsController : ControllerBase
+{
+    private readonly InternService _internService;
+
+    public InternsController(InternService internService)
+    {
+        _internService = internService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Intern>>> GetAll()
+    {
+        return Ok(await _internService.GetAllInternsAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Intern>> GetById(int id)
+    {
+        var intern = await _internService.GetInternByIdAsync(id);
+        if (intern == null)
+        {
+            return NotFound();
+        }
+        return Ok(intern);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Intern>> Create(Intern intern)
+    {
+        await _internService.AddInternAsync(intern);
+        return CreatedAtAction(nameof(GetById), new { id = intern.Id }, intern);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Intern intern)
+    {
+        if (id != intern.Id)
+        {
+            return BadRequest();
+        }
+
+        await _internService.UpdateInternAsync(intern);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _internService.DeleteInternAsync(id);
+        return NoContent();
+    }
+}
---
+++ task_for_66bit/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+using task_for_66bit.Data.Models;
+using task_for_66bit.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace task_for_66bit.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly ProjectService _projectService;
+
+        public ProjectsController(ProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetAll()
+        {
+            return Ok(await _projectService.GetAllProjectsAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetById(int id)
+        {
+            var project = await _projectService.GetProjectByIdAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Project>> Create(Project project)
+        {
+            await _projectService.AddProjectAsync(project);
+            return CreatedAtAction(nameof(GetById), new { id = project.Id }, project);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            await _projectService.UpdateProjectAsync(project);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _projectService.DeleteProjectAsync(id);
+            return NoContent();
+        }
+    }
+}
